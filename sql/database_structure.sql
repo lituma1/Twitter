58f7332de722fbb856cf02300df6e75f20676376
Piotr Popkiewicz <lituma1@gmail.com>
2016-11-26 14:27:48 +0100

    Modify database_sturcture.sql and Tweet.php

--- sql/database_structure.sql
+++ sql/database_structure.sql
@@ -9,11 +9,18 @@
  */
 
 CREATE DATABASE twitter; 
+
 CREATE TABLE Users (id INT unsigned NOT NULL auto_increment, email VARCHAR(255) UNIQUE, username VARCHAR(255), hashed_password VARCHAR(255), PRIMARY KEY(id));
-CREATE TABLE Tweet (id INT unsigned NOT NULL auto_increment, user_id INT unsigned NOT NULL, tweet_text VARCHAR(140), PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES Users(id) ON DELETE CASCADE);
-ALTER TABLE Tweet ADD creation_date DATETIME;
 INSERT INTO Users (username, email, hashed_password) VALUES('$this->username', '$this->email', '$this->hashedPassword');
 UPDATE Users SET username = '$this->username', email = '$this->email',hashed_password = '$this->hashedPassword' WHERE id= $this->id;
 SELECT * FROM Users
 SELECT id, username, email, hashed_password FROM Users WHERE id=$id;
 DELETE FROM Users WHERE id=$this->id;
+
+
+CREATE TABLE Tweet (id INT unsigned NOT NULL auto_increment, user_id INT unsigned NOT NULL, tweet_text VARCHAR(140), PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES Users(id) ON DELETE CASCADE);
+ALTER TABLE Tweet ADD creation_date DATETIME;
+INSERT INTO Tweet (user_id, tweet_text, creation_date) VALUES ('$this->userId', '$this->text', '$this->creationDate');
+SELECT * FROM Tweet ORDER BY creation_date DESC;
+SELECT * FROM Tweet WHERE id = $id;
+SELECT * FROM Tweet WHERE user_id= $userId ORDER BY creation_date DESC;
--- src/Tweet.php
+++ src/Tweet.php
@@ -11,35 +11,40 @@
  *
  * @author pp
  */
-include_once '../web/connection.php';
+//include_once '../web/connection.php';
+
 class Tweet {
+
     //put your code here
     private $id;
     private $userId;
     private $text;
     private $creationDate;
+
     function __construct() {
         $this->id = -1;
         $this->userId = '';
         $this->text = '';
         $this->creationDate = '';
     }
+
     function setUserId($userId) {
         $this->userId = $userId;
     }
 
     function setText($text) {
-        if(strlen($text <= 140)){
-            $this->text = $text;
-        } else {
-        echo 'Twój tweet jest za długi maksymalna długość znaków to 140';    
-        }
-        
+        $this->text = $text;
+//        if(strlen($text <= 140)){
+//            $this->text = $text;
+//        } else {
+//        echo 'Twój tweet jest za długi maksymalna długość znaków to 140';    
+//        }
     }
 
     function setCreationDate($creationDate) {
         $this->creationDate = $creationDate;
     }
+
     function getId() {
         return $this->id;
     }
@@ -56,33 +61,98 @@ class Tweet {
         return $this->creationDate;
     }
 
-    static function loadTweetById(mysqli $connection, $id){
-       
+    static function loadAllTweets(mysqli $connection) {
+        $ret = [];
+        $sql = "SELECT * FROM Tweet ORDER BY creation_date DESC";
+        $result = $connection->query($sql);
+        if ($result && $result->num_rows > 0) {
+            foreach ($result as $row) {
+                $loadedTweet = new Tweet();
+                $loadedTweet->id = $row['id'];
+                $loadedTweet->setUserId($row['user_id']);
+                $loadedTweet->setText($row['tweet_text']);
+                $loadedTweet->setCreationDate($row['creation_date']);
+                $ret[] = $loadedTweet;
+            }
+        }
+        return $ret;
     }
-    static function loadAllTweetsByUserId(mysqli $connection, $userId){
-        
-    }
-    function saveToDb(mysqli $connection){
-        if($this->id == -1){
-            $sql = "INSERT INTO Tweet (user_id, tweet_text, creation_date) VALUES ('$this->userId', '$this->text', '$this->creationDate')";
-            
-                    
-            $result = $connection->query($sql);
-            if($result){
-                $this->id = $connection->insert_id;
-                return true;
+
+    static function loadTweetById(mysqli $connection, $id) {
+
+        $sql = "SELECT * FROM Tweet WHERE id = $id";
+        $result = $connection->query($sql);
+        if ($result) {
+            if ($result->num_rows != 0) {
+                foreach ($result as $row) {
+                    $loadedTweet = new Tweet();
+                    $loadedTweet->id = $row['id'];
+                    $loadedTweet->setUserId($row['user_id']);
+                    $loadedTweet->setText($row['tweet_text']);
+                    $loadedTweet->setCreationDate($row['creation_date']);
+                }
+                return $loadedTweet;
             } else {
-            echo 'nie dodano tweeta do bazy'.$connection->error;    
+                echo 'tweet o podanym id nie istnieje';
+            }
+        } else {
+            echo 'błędne zapytanie do bazy' . $connection->error;
+        }
+    }
+
+    static function loadAllTweetsByUserId(mysqli $connection, $userId) {
+        $sql = "SELECT * FROM Tweet WHERE user_id= $userId ORDER BY creation_date DESC";
+        $result = $connection->query($sql);
+        $ret = [];
+        if($result){
+            if($result->num_rows !=0){
+                foreach ($result as $row){
+                $loadedTweet = new Tweet();
+                    $loadedTweet->id = $row['id'];
+                    $loadedTweet->setUserId($row['user_id']);
+                    $loadedTweet->setText($row['tweet_text']);
+                    $loadedTweet->setCreationDate($row['creation_date']);
+                    $ret[] = $loadedTweet;
+                }
+                return $ret;
+            } else {
+            echo 'użytkownik o podanym id nie wysłał jeszcze tweetów';    
+            }
+        } else {
+        echo 'błędne zapytanie do bazy '.$connection->error;    
+        }
+    }
+
+    function saveToDb(mysqli $connection) {
+        if ($this->id == -1) {
+            $sql = "INSERT INTO Tweet (user_id, tweet_text, creation_date) VALUES ('$this->userId', '$this->text', '$this->creationDate')";
+
+            if (!empty($this->text)) {
+                $result = $connection->query($sql);
+                if ($result) {
+                    $this->id = $connection->insert_id;
+                    return true;
+                } else {
+                    echo 'nie dodano tweeta do bazy' . $connection->error;
+                }
+            } else {
+                echo 'nie dodajemy pustych tweetów do bazy';
             }
         }
         return false;
-    } 
+    }
+
 }
 
-$tweet = new Tweet();
-$tweet->setText('Tak naprawdę piękna pogoda');
-$tweet->setUserId(59);
-$tweet->setCreationDate(date('Y-m-d H:i:s'));
-var_dump($tweet);
-$tweet->saveToDb($connection);
+//$tweet = new Tweet();
+//$tweet->setText('');
+//$tweet->setUserId(26);
+//$tweet->setCreationDate(date('Y-m-d H:i:s'));
+//var_dump($tweet);
+//$tweet->saveToDb($connection);
+//$array = Tweet::loadAllTweets($connection);
+//var_dump($array);
+//$tweet = Tweet::loadAllTweets($connection);
+//var_dump($tweet);
+
 
